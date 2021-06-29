1bf774e58ee7488e4c1471ae256239f071833044
Abdullah Saqer <a.saqer@arabot.io>
2021-06-29 23:46:03 +0300

    first task

--- bin/basics.dart
+++ bin/basics.dart
@@ -1,4 +1,18 @@
 import 'dart:io';
-void main(List<String> arguments) {
 
+void main(List<String> arguments) {
+  var input = stdin.readLineSync();
+  var number = int.parse(input);
+  var result = '${number}';
+  if(number % 2 == 0) {
+    result += ' Even';
+  } else {
+    result += ' Odd';
+  }
+  if(number >= 0) {
+    result += ' Positive';
+  } else {
+    result += ' Nigative';
+  }
+  print(result);
 }
